99aeaad3ce71b25d2c7f3491a56eef539531a34f
Lucas Yang <lyang420@terpmail.umd.edu>
2023-07-03 14:39:23 -0400

    generated plot

--- no2-unadjusted.py
+++ no2-unadjusted.py
@@ -3,8 +3,8 @@
 # the census block level as seen in Figure 1(a).
 from createdf import init
 
+import matplotlib.pyplot as plt
 import numpy as np
-import statistics as stat
 
 df = init()
 
@@ -18,6 +18,9 @@ no2_black     = []
 no2_asian     = []
 no2_hispanic  = []
 
+# The population-weighted distribution of NO2 concentrations by HOLC grade
+# appears identical to that shown in Figure 1(a). However, similar approach
+# to plot distribution by race/ethnicity yields different numbers.
 for i in df.index:
    if df['Grade'][i] == 'A':
       no2_A += (df['Total'][i] * [df['NO2'][i]])
@@ -27,6 +30,8 @@ for i in df.index:
       no2_C += (df['Total'][i] * [df['NO2'][i]])
    if df['Grade'][i] == 'D':
       no2_D += (df['Total'][i] * [df['NO2'][i]])
+   # See here: Population-weighted distribution of NO2 levels by ethnicity
+   # differs non-trivially from the figures displayed in the study.
    if df['White'][i] > 0:
       no2_white += (df['White'][i] * [df['NO2'][i]])
    if df['Other'][i] > 0:
@@ -37,3 +42,20 @@ for i in df.index:
       no2_asian += (df['Asian'][i] * [df['NO2'][i]])
    if df['Hispanic'][i] > 0:
       no2_hispanic += (df['Hispanic'][i] * [df['NO2'][i]])
+
+data = [no2_A, no2_B, no2_C, no2_D, no2_white, no2_other, no2_black, no2_asian, no2_hispanic]
+labels = ['A', 'B', 'C', 'D', 'White', 'Other', 'Black', 'Asian', 'Hispanic']
+
+fig1, ax1 = plt.subplots(nrows = 1, ncols = 1)
+no2_plot = ax1.boxplot(data, labels = labels, patch_artist = True, showfliers = False, vert = True, whis = 0)
+ax1.set_title('NO2 (ppb) by HOLC and Race/Ethnicity')
+
+ax1.set_ylim(0, 25)
+for median in no2_plot['medians']:
+   median.set_color('black')
+
+colors = ['lightcoral', 'burlywood', 'lightgreen', 'lightskyblue', 'firebrick', 'mediumpurple', 'goldenrod', 'darkseagreen', 'cornflowerblue']
+for patch, color in zip(no2_plot['boxes'], colors):
+   patch.set_facecolor(color)
+
+plt.savefig('no2-unadjusted.png', dpi = 300)
