9459f2d25da30cbf5e85379f613cf1a1b91e6307
Lucas Yang <lyang420@terpmail.umd.edu>
2023-07-05 14:18:51 -0400

    added pm25 unadjusted figures

---
+++ pm25-unadjusted.py
@@ -0,0 +1,56 @@
+# This script aims to create a plot displaying the unadjusted national
+# population-weighted distributions of PM25 levels within HOLC-mapped areas at
+# the census block level as seen in Figure 1(b).
+from createdf import init
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = init()
+
+pm25_A         = []
+pm25_B         = []
+pm25_C         = []
+pm25_D         = []
+pm25_white     = []
+pm25_other     = []
+pm25_black     = []
+pm25_asian     = []
+pm25_hispanic  = []
+
+for i in df.index:
+   if df['Grade'][i] == 'A':
+      pm25_A += (df['Total'][i] * [df['PM25'][i]])
+   if df['Grade'][i] == 'B':
+      pm25_B += (df['Total'][i] * [df['PM25'][i]])
+   if df['Grade'][i] == 'C':
+      pm25_C += (df['Total'][i] * [df['PM25'][i]])
+   if df['Grade'][i] == 'D':
+      pm25_D += (df['Total'][i] * [df['PM25'][i]])
+   if (df['White'][i] > 0):
+      pm25_white += (df['White'][i] * [df['PM25'][i]])
+   if (df['Other'][i] > 0):
+      pm25_other += (df['Other'][i] * [df['PM25'][i]])
+   if (df['Black'][i] > 0):
+      pm25_black += (df['Black'][i] * [df['PM25'][i]])
+   if (df['Asian'][i] > 0):
+      pm25_asian += (df['Asian'][i] * [df['PM25'][i]])
+   if (df['Hispanic'][i] > 0):
+      pm25_hispanic += (df['Hispanic'][i] * [df['PM25'][i]])
+
+data = [pm25_A, pm25_B, pm25_C, pm25_D, pm25_white, pm25_other, pm25_black, pm25_asian, pm25_hispanic]
+labels = ['A', 'B', 'C', 'D', 'White', 'Other', 'Black', 'Asian', 'Hispanic']
+
+fig1, ax1 = plt.subplots(nrows = 1, ncols = 1)
+pm25_plot = ax1.boxplot(data, labels = labels, patch_artist = True, showfliers = False, vert = True, whis = 0)
+ax1.set_title('Unadjusted: national aggregation')
+
+ax1.set_ylim(0, 15)
+for median in pm25_plot['medians']:
+   median.set_color('black')
+
+colors = ['forestgreen', 'cornflowerblue', 'goldenrod', 'firebrick', 'palevioletred', 'darkkhaki', 'plum', 'slateblue', 'mediumseagreen']
+for patch, color in zip(pm25_plot['boxes'], colors):
+   patch.set_facecolor(color)
+
+plt.savefig('pm25-unadjusted.png', dpi = 300)
