5c25b8a0d3d1a1638e874cece4291efcb7024b95
Lucas Yang <lyang420@terpmail.umd.edu>
2023-07-06 14:08:38 -0400

    fixed PM25 adjusted plot

--- pm25-adjusted.py
+++ pm25-adjusted.py
@@ -42,30 +42,27 @@ for i in df.index:
       pm25_C += (df['Total'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
    if df['Grade'][i] == 'D':
       pm25_D += (df['Total'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
-   if df['White'][i] > 0:
+   if df['Grade'][i] != 'N':
       pm25_white += (df['White'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
-   if df['Other'][i] > 0:
       pm25_other += (df['Other'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
-   if df['Black'][i] > 0:
       pm25_black += (df['Black'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
-   if df['Asian'][i] > 0:
       pm25_asian += (df['Asian'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
-   if df['Hispanic'][i] > 0:
       pm25_hispanic += (df['Hispanic'][i] * [(df['PM25'][i] - pwms[df['City'][i]])])
 
 data = [pm25_A, pm25_B, pm25_C, pm25_D, pm25_white, pm25_other, pm25_black, pm25_asian, pm25_hispanic]
 labels = ['A', 'B', 'C', 'D', 'White', 'Other', 'Black', 'Asian', 'Hispanic']
 
-fig1, ax1 = plt.subplots(nrows = 1, ncols = 1)
-pm25_plot = ax1.boxplot(data, labels = labels, patch_artist = True, showfliers = False, vert = True, whis = 0)
-ax1.set_title('Adjusted: intraurban variation only')
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+plot = ax.boxplot(data, labels = labels, patch_artist = True, showfliers = False, vert = True, whis = 0)
+ax.set_title('Adjusted: Intraurban Variation')
 
-ax1.set_ylim(-1.2, 1.2)
-for median in pm25_plot['medians']:
+ax.set_ylim(-0.8, 0.6)
+for median in plot['medians']:
    median.set_color('black')
 
-colors = ['forestgreen', 'cornflowerblue', 'goldenrod', 'firebrick', 'palevioletred', 'darkkhaki', 'plum', 'slateblue', 'mediumseagreen']
-for patch, color in zip(pm25_plot['boxes'], colors):
+colors = ['lightcoral', 'burlywood', 'lightgreen', 'lightskyblue',
+          'firebrick', 'orchid', 'darkkhaki', 'darkseagreen', 'cornflowerblue']
+for patch, color in zip(plot['boxes'], colors):
    patch.set_facecolor(color)
 
 plt.savefig('pm25-adjusted.png', dpi = 300)
