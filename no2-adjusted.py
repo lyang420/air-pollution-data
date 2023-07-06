4b80b21af19c2f7085da7d434b8ccdc277d1a778
Lucas Yang <lyang420@terpmail.umd.edu>
2023-07-06 13:56:30 -0400

    fixed NO2 adjusted plot

--- no2-adjusted.py
+++ no2-adjusted.py
@@ -44,30 +44,27 @@ for i in df.index:
       no2_C += (df['Total'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
    if df['Grade'][i] == 'D':
       no2_D += (df['Total'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
-   if df['White'][i] > 0:
+   if df['Grade'][i] != 'N':
       no2_white += (df['White'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
-   if df['Other'][i] > 0:
       no2_other += (df['Other'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
-   if df['Black'][i] > 0:
       no2_black += (df['Black'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
-   if df['Asian'][i] > 0:
       no2_asian += (df['Asian'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
-   if df['Hispanic'][i] > 0:
       no2_hispanic += (df['Hispanic'][i] * [(df['NO2'][i] - pwms[df['City'][i]])])
 
 data = [no2_A, no2_B, no2_C, no2_D, no2_white, no2_other, no2_black, no2_asian, no2_hispanic]
 labels = ['A', 'B', 'C', 'D', 'White', 'Other', 'Black', 'Asian', 'Hispanic']
 
-fig1, ax1 = plt.subplots(nrows = 1, ncols = 1)
-no2_plot = ax1.boxplot(data, labels = labels, patch_artist = True, showfliers = False, vert = True, whis = 0)
-ax1.set_title('Adjusted: intraurban variation only')
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+plot = ax.boxplot(data, labels = labels, patch_artist = True, showfliers = False, vert = True, whis = 0)
+ax.set_title('Adjusted: Intraurban Variation')
 
-ax1.set_ylim(-4, 4)
-for median in no2_plot['medians']:
+ax.set_ylim(-4, 3)
+for median in plot['medians']:
    median.set_color('black')
 
-colors = ['forestgreen', 'cornflowerblue', 'goldenrod', 'firebrick', 'palevioletred', 'darkkhaki', 'plum', 'slateblue', 'mediumseagreen']
-for patch, color in zip(no2_plot['boxes'], colors):
+colors = ['lightcoral', 'burlywood', 'lightgreen', 'lightskyblue',
+          'firebrick', 'orchid', 'darkkhaki', 'darkseagreen', 'cornflowerblue']
+for patch, color in zip(plot['boxes'], colors):
    patch.set_facecolor(color)
 
 plt.savefig('no2-adjusted.png', dpi = 300)
