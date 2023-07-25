0291140201d4e3c876eaa74e4d5e2962ce02a68d
Lucas Yang <lyang420@umd.edu>
2023-07-25 18:39:53 -0400

    finished abstracting and editing Figure S4

---
+++ figure-s4/figure-s4.py
@@ -0,0 +1,35 @@
+from utils import init
+from utils import collect_data_percentile
+
+import matplotlib.pyplot as plt
+
+def generate_plot(data, percentiles, title, x_label, y_label, y_min, y_max, y_ticks, file_name):
+   fig, ax = plt.subplots()
+   l1 = plt.scatter(percentiles, data[0], s = 5, label = 'HOLC Grade A')
+   l2 = plt.scatter(percentiles, data[1], s = 5, label = 'HOLC Grade B')
+   l3 = plt.scatter(percentiles, data[2], s = 5, label = 'HOLC Grade C')
+   l4 = plt.scatter(percentiles, data[3], s = 5, label = 'HOLC Grade D')
+   l5 = plt.scatter(percentiles, data[4], s = 5, label = 'CUA (Census Urbanized Area)')
+   ax.set_title(title)
+   ax.set_xlabel(x_label)
+   ax.set_ylabel(y_label)
+   ax.set_ylim(y_min, y_max)
+   ax.set_yticks(y_ticks)
+   ax.legend()
+   ax.set_axisbelow(True)
+   ax.grid(color = "gainsboro")
+   ax.figure.set_figwidth(5)
+   plt.savefig(file_name, dpi = 300)
+
+df = init()
+
+no2_data = collect_data_percentile(df, 'PHOLC', 'Total', 'NO2')
+pm25_data = collect_data_percentile(df, 'PHOLC', 'Total', 'PM25')
+percentiles = range(0, 100)
+
+generate_plot(no2_data, percentiles, 'Overall Cumulative NO₂ Distribution',
+              'Population Percentile', 'NO₂ (ppb)', 0, 35, (0, 10, 20, 30),
+              'figure-s4-a.png')
+generate_plot(pm25_data, percentiles, 'Overall Cumulative PM₂.₅ Distribution',
+              'Population Percentile', 'PM₂.₅ (μg/m³)', 0, 16, (4, 8, 12, 16),
+              'figure-s4-b.png')
