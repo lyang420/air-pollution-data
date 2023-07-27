58262a6fce6187e87231dd7b0b8b39d44541b6c2
Lucas Yang <lyang420@umd.edu>
2023-07-27 17:24:03 -0400

    moved a few variables around via indentation

--- figure-s4/figure-s4.py
+++ figure-s4/figure-s4.py
@@ -3,7 +3,8 @@ from utils import collect_data_percentile
 
 import matplotlib.pyplot as plt
 
-def generate_plot(data, percentiles, title, x_label, y_label, y_min, y_max, x_tick_loc, x_ticks, y_ticks, file_name):
+def generate_plot(data, percentiles, title, x_label, y_label, y_min, y_max,
+                  x_tick_loc, x_ticks, y_ticks, file_name):
    fig, ax = plt.subplots()
    l1 = plt.scatter(percentiles, data[0], s = 5, label = 'HOLC Grade A')
    l2 = plt.scatter(percentiles, data[1], s = 5, label = 'HOLC Grade B')
@@ -30,8 +31,8 @@ percentiles = range(0, 100)
 ticks = (0.01, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99.99)
 
 generate_plot(no2_data, percentiles, 'Overall Cumulative NO₂ Distribution',
-              'Population Percentile', 'NO₂ (ppb)', 0, 35, ticks, ticks, (0, 10, 20, 30),
-              'figure-s4-a.png')
+              'Population Percentile', 'NO₂ (ppb)', 0, 35, ticks, ticks,
+              (0, 10, 20, 30), 'figure-s4-a.png')
 generate_plot(pm25_data, percentiles, 'Overall Cumulative PM₂.₅ Distribution',
-              'Population Percentile', 'PM₂.₅ (μg/m³)', 0, 16, ticks, ticks, (4, 8, 12, 16),
-              'figure-s4-b.png')
+              'Population Percentile', 'PM₂.₅ (μg/m³)', 0, 16, ticks, ticks,
+              (4, 8, 12, 16), 'figure-s4-b.png')
