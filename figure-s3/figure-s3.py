f3587a8b378b0437cb3229f5a76b2721dc347203
Lucas Yang <lyang420@umd.edu>
2023-07-31 12:15:20 -0400

    made some final edits to indentation, variables, and comments for figure-s3.py.

--- figure-s3/figure-s3.py
+++ figure-s3/figure-s3.py
@@ -1,9 +1,17 @@
+# `figure-s3.py` generates the two plots from Figure S3, shown in the study's
+# supporting information file. These plots show the demographics of the
+# residents sampled in the raw data.
+
 from utils import init
 from utils import collect_data_residents
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+# `generate_plot` abstracts the plot generation process and allows users to
+# customize the input data, as well as cosmetic details of each plot, such as
+# the labels to use, the title to name the plot, as well as the colors and
+# file name.
 def generate_plot(weights, width, title, x_label, y_label, y_tick_loc, y_ticks,
                   file_name):
    fig, ax = plt.subplots()
@@ -18,27 +26,31 @@ def generate_plot(weights, width, title, x_label, y_label, y_tick_loc, y_ticks,
    ax.legend(loc = "upper right")
    plt.savefig(file_name, dpi = 300)
 
+# Initialize DataFrame
 df = init()
 
+# Collect data.
 numeric_data, percentage_data = collect_data_residents(df, 'PHOLC', 1000000.0)
 grades = ("A", "B", "C", "D")
 
+# Organize data to be fed into matplotlib.
 numeric_weights = {
-   "White": numeric_data[0],
-   "Other": numeric_data[1],
-   "Black": numeric_data[2],
-   "Asian": numeric_data[3],
-   "Hispanic": numeric_data[4]
+   "White"    : numeric_data[0],
+   "Other"    : numeric_data[1],
+   "Black"    : numeric_data[2],
+   "Asian"    : numeric_data[3],
+   "Hispanic" : numeric_data[4]
 }
 
 percentage_weights = {
-   "White": percentage_data[0],
-   "Other": percentage_data[1],
-   "Black": percentage_data[2],
-   "Asian": percentage_data[3],
-   "Hispanic": percentage_data[4]
+   "White"    : percentage_data[0],
+   "Other"    : percentage_data[1],
+   "Black"    : percentage_data[2],
+   "Asian"    : percentage_data[3],
+   "Hispanic" : percentage_data[4]
 }
 
+# Generate the two plots.
 generate_plot(numeric_weights, 0.65, 'Demographics in HOLC-Mapped Areas',
               'HOLC Grade', 'Population (Millions)', (0, 5, 10, 15, 20),
               (0, 5, 10, 15, 20), 'figure-s3-a.png')
