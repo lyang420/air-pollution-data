ee3b86d0a6391b89155ba91bb8cddfcc0c86dddc
Lucas Yang <lyang420@umd.edu>
2023-07-31 12:13:18 -0400

    added some comments for clarity in figure-2.py.

--- figure-2/figure-2.py
+++ figure-2/figure-2.py
@@ -1,8 +1,17 @@
+# `figure-2.py` generates the two plots from Figure 2 in the study, which
+# displays  NO₂ levels (measured in ppb) and PM₂.₅ levels (measured in μg/m³)
+# for each ethnicity in the study. Each level is also divided by the HOLC grade
+# of the block the residents are living in.
+
 from utils import init
 from utils import collect_data_intraurban_diff
 
 import matplotlib.pyplot as plt
 
+# `generate_plot` abstracts the plot generation process and allows users to
+# customize the input data, as well as cosmetic details of each plot, such as
+# the labels to use, the title to name the plot, as well as the colors and
+# file name.
 def generate_plot(data, axes, x_label, y_label, y_min, y_max, y_ticks, file_name):
    fig, ax = plt.subplots()
    ax.plot(axes, data[0], 'g--', label = 'Hispanic')
@@ -18,13 +27,16 @@ def generate_plot(data, axes, x_label, y_label, y_min, y_max, y_ticks, file_name
    ax.legend()
    plt.savefig(file_name, dpi = 300)
 
+# Initialize DataFrame
 df = init()
 
+# Collect data.
 no2_intraurban_diff  = collect_data_intraurban_diff(df, 'PHOLC', 'NO2')
 pm25_intraurban_diff = collect_data_intraurban_diff(df, 'PHOLC', 'PM25')
 
 axes = ['A', 'B', 'C', 'D']
 
+# Generate the two plots.
 generate_plot(no2_intraurban_diff, axes, 'HOLC Grade',
               'Intraurban NO₂ Difference (ppb)', -2.5, 1.5, (-2, -1, 0, 1),
               'figure-2-a.png')
